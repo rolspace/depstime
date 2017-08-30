157b8965a7fab59786f04b5f88b463d4b6b5ae1e
Rolando Ramos <rolandorramos@gmail.com>
2017-08-31 01:14:35 +0200

    Converted main function to be Promise based

--- src/index.js
+++ src/index.js
@@ -3,28 +3,30 @@ import path from 'path'
 import jsonfile from 'jsonfile'
 import { Console } from 'console'
 
-export default function depstime(directory, cb) {
-	const logger = new Console(process.stdout, process.stderr)
+export default function depstime(directory) {
+	return new Promise((resolve, reject) => {
+		const logger = new Console(process.stdout, process.stderr)
 
-	if (fs.existsSync(directory)) {
-		const packageJsonPath = path.join(directory, 'package.json')
+		if (fs.existsSync(directory)) {
+			const packageJsonPath = path.join(directory, 'package.json')
 
-		jsonfile.readFile(packageJsonPath, (error, obj) => {
-			if (error) {
-				logger.error(`Path ${directory} does not have a package.json file.`)
-				cb(false)
-			}
-			else {
-				if (!obj.hasOwnProperty('dependencies') && !obj.hasOwnProperty('devDependencies')) {
-					logger.info('There are no dependencies in the package.json file.')
-					cb(false)
+			jsonfile.readFile(packageJsonPath, (error, obj) => {
+				if (error) {
+					logger.error(`Path ${directory} does not have a package.json file.`)
+					reject(false)
 				}
+				else {
+					if (!obj.hasOwnProperty('dependencies') && !obj.hasOwnProperty('devDependencies')) {
+						logger.info('There are no dependencies in the package.json file.')
+						reject(false)
+					}
 
-				cb(true)
-			}
-		})
-	}
+					resolve(true)
+				}
+			})
+		}
 
-	logger.error(`Path ${directory} does not exist.`)
-	cb(false)
+		logger.error(`Path ${directory} does not exist.`)
+		reject(false)
+	})
 }
--- tests/index-test.js
+++ tests/index-test.js
@@ -2,9 +2,13 @@ import fs from 'fs'
 import path from 'path'
 import jsonfile from 'jsonfile'
 import { Console } from 'console'
-import { expect } from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised'
 import sinon from 'sinon';
 import depstime from '../dist/index'
+
+chai.use(chaiAsPromised)
+const expect = chai.expect
 
 describe('depstime', () => {
 	let loggerInfoStub, loggerErrorStub
@@ -20,61 +24,73 @@ describe('depstime', () => {
 	})
 
 	it('Callback called with false argument if the path does not exist', () => {
-		const cb = sinon.spy()
 		const fsStub = sinon.stub(fs, 'existsSync').returns(false)
 
-		depstime('/path/that/does/not/exist', cb)
+		const result = depstime('/path/that/does/not/exist')
 
 		fsStub.restore()
 
-		expect(cb.calledWith(false)).to.equal(true)
+		return expect(result).to.be.rejected.and.to.eventually.equal(false)
 	})
 
 	it('Callback called with false argument if the path does not contain a package.json file', () => {
-		const cb = sinon.spy()
-
 		const fsStub = sinon.stub(fs, 'existsSync').returns(true)
 		const pathStub = sinon.stub(path, 'join').returns('/path/that/does/exist/package.json')
 		const jsonfileStub = sinon.stub(jsonfile, 'readFile').yields('error', null)
 
-		const result = depstime('/path/that/does/exist', cb)
+		const result = depstime('/path/that/does/exist')
 
 		fsStub.restore()
 		pathStub.restore()
 		jsonfileStub.restore()
 
-		expect(cb.calledWith(false)).to.equal(true)
+		return expect(result).to.be.rejected.and.to.eventually.equal(false)
 	})
 
 	it('Callback called with false argument if the package.json file does not have dependencies', () => {
-		const cb = sinon.spy()
-
 		const fsStub = sinon.stub(fs, 'existsSync').returns(true)
 		const pathStub = sinon.stub(path, 'join').returns('/path/that/does/exist/package.json')
 		const jsonfileStub = sinon.stub(jsonfile, 'readFile').yields(null, { name: 'depstime'  })
 
-		depstime('/path/that/does/exist', cb)
+		const result = depstime('/path/that/does/exist')
 
 		fsStub.restore()
 		pathStub.restore()
 		jsonfileStub.restore()
 
-		expect(cb.calledWith(false)).to.equal(true)
+		return expect(result).to.be.rejected.and.to.eventually.equal(false)
 	})
 
 	it('Returns an object with each dependency as an object, dependencies are ordered alphabetically', () => {
-		const cb = sinon.spy();
+		const packageJson = {
+			name: 'depstime',
+			dependencies: {
+				dep1: '^1.2.1'
+			},
+			devDependencies: {
+				version: '3.0.0'
+			}
+		}
 
 		const fsStub = sinon.stub(fs, 'existsSync').returns(true)
 		const pathStub = sinon.stub(path, 'join').returns('/path/that/does/exist/package.json')
-		const jsonfileStub = sinon.stub(jsonfile, 'readFile').yields(null, { name: 'depstime', dependencies: { dep: '^1.2.1' } })
+		const jsonfileStub = sinon.stub(jsonfile, 'readFile').yields(null, packageJson)
 
-		const result = depstime('/path/that/does/exist', cb)
+		const expected = {
+			dep: {
+				version: '^1.2.1'
+			},
+			devDep: {
+				version: '3.0.0'
+			}
+		}
+
+		const result = depstime('/path/that/does/exist')
 
 		fsStub.restore()
 		pathStub.restore()
 		jsonfileStub.restore()
 
-		expect(cb.calledWith(false)).to.equal(true)
+		return expect(result).to.be.fulfilled.and.to.eventually.deep.equal(expected)
 	})
 })

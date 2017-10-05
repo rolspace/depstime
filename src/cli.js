97941b5150807ed5d32b26cd20f4c06a95a0749c
Rolando Ramos <rolandorramos@gmail.com>
2017-10-05 22:46:17 +0200

    Included statement to output promise resolution result

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,5 @@ import depstime from './index'
 
 export default function cli() {
 	const argv = minimist(process.argv.slice(2));
-	depstime(argv._[0])
+	depstime(argv._[0]).then(value => console.log(value));
 }

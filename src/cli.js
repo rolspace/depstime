68b63f922826793c7d38902693a27aa5f6b1d001
Rolando Ramos <rolandorramos@gmail.com>
2018-01-04 20:14:32 +0100

    Passed additional argument to depstime call for new parameters.

--- src/cli.js
+++ src/cli.js
@@ -3,8 +3,9 @@ import minimist from 'minimist'
 import depstime from './index'
 
 export default function cli() {
-	const argv = minimist(process.argv.slice(2));
-	depstime(argv._[0])
+	const argv = minimist(process.argv.slice(2))
+
+	depstime(argv._[0], argv)
 	.then(value => console.log(util.inspect(value, { colors: true, depth: null })))
-	.catch(error => console.log(error));
+	.catch(error => console.log(error))
 }

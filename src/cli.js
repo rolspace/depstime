389c5c5f82f85c31ee6ca79796ab0abac4a3653f
Rolando Ramos <rolandorramos@gmail.com>
2018-01-07 20:55:34 +0100

    Modified cli module to support yargs to read options

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,20 @@
 import util from 'util'
+import yargs from 'yargs'
 import minimist from 'minimist'
 import depstime from './index'
 
 export default function cli() {
-	const argv = minimist(process.argv.slice(2))
+	const options = yargs
+		.usage('$0 [directory]')
+		.option('human', {
+      alias: 'h',
+      describe: 'Output time difference as a human readable value',
+      type: 'boolean'
+    })
+		.help('help', 'Show help message')
+		.argv
 
-	depstime(argv._[0], argv)
-	.then(value => console.log(util.inspect(value, { colors: true, depth: null })))
-	.catch(error => console.log(error))
+		depstime(options._[0], options)
+		.then(value => console.log(util.inspect(value, { colors: true, depth: null })))
+		.catch(error => console.log(error))
 }

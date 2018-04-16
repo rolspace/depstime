25f67653fdb5fcd3a55c3bee4b52ba73e9e204e8
Rolando Ramos <rolandorramos@gmail.com>
2018-04-16 21:41:55 +0200

    Latest application built

--- dist/cli.js
+++ dist/cli.js
@@ -1,39 +1,35 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 exports.default = cli;
 
-var _util = require('util');
+var _util = _interopRequireDefault(require("util"));
 
-var _util2 = _interopRequireDefault(_util);
+var _yargs = _interopRequireDefault(require("yargs"));
 
-var _yargs = require('yargs');
+var _minimist = _interopRequireDefault(require("minimist"));
 
-var _yargs2 = _interopRequireDefault(_yargs);
-
-var _minimist = require('minimist');
-
-var _minimist2 = _interopRequireDefault(_minimist);
-
-var _index = require('./index');
-
-var _index2 = _interopRequireDefault(_index);
+var _index = _interopRequireDefault(require("./index"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function cli() {
-	var options = _yargs2.default.usage('$0 [directory]').option('human', {
-		alias: 'h',
-		describe: 'Output time difference as a human readable value',
-		type: 'boolean'
-	}).help('help', 'Show help message').argv;
+  const options = _yargs.default.usage('$0 [directory]').option('human-compact', {
+    alias: 'c',
+    describe: 'Output time difference as a human readable value up to the \'days\' unit with rounding',
+    type: 'boolean'
+  }).option('human-full', {
+    alias: 'f',
+    describe: 'Output time difference as a human readable value up to the \'seconds\' unit with rounding',
+    type: 'boolean'
+  }).help('help', 'Show help message').argv;
 
-	(0, _index2.default)(options._[0], options).then(function (value) {
-		return console.log(_util2.default.inspect(value, { colors: true, depth: null }));
-	}).catch(function (error) {
-		return console.log(error);
-	});
+  (0, _index.default)(options._[0], options).then(value => console.log(_util.default.inspect(value, {
+    colors: true,
+    depth: null
+  }))).catch(error => console.log(error));
 }
-module.exports = exports['default'];
+
+module.exports = exports["default"];
--- dist/index.js
+++ dist/index.js
@@ -1,137 +1,133 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 exports.default = depstime;
 
-var _fs = require('fs');
+var _fs = _interopRequireDefault(require("fs"));
 
-var _fs2 = _interopRequireDefault(_fs);
+var _path = _interopRequireDefault(require("path"));
 
-var _path = require('path');
+var _jsonfile = _interopRequireDefault(require("jsonfile"));
 
-var _path2 = _interopRequireDefault(_path);
+var _semver = _interopRequireDefault(require("semver"));
 
-var _jsonfile = require('jsonfile');
+var _moment = _interopRequireDefault(require("moment"));
 
-var _jsonfile2 = _interopRequireDefault(_jsonfile);
+var _humanizeDuration = _interopRequireDefault(require("humanize-duration"));
 
-var _semver = require('semver');
-
-var _semver2 = _interopRequireDefault(_semver);
-
-var _moment = require('moment');
-
-var _moment2 = _interopRequireDefault(_moment);
-
-var _humanizeDuration = require('humanize-duration');
-
-var _humanizeDuration2 = _interopRequireDefault(_humanizeDuration);
-
-var _child_process = require('child_process');
+var _child_process = require("child_process");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function depstime(directory, options) {
-	return new Promise(function (resolve, reject) {
-		if (!directory) {
-			directory = process.cwd();
-		}
+  return new Promise((resolve, reject) => {
+    if (!directory) {
+      directory = process.cwd();
+    }
 
-		if (_fs2.default.existsSync(directory)) {
-			var packageJsonPath = _path2.default.join(directory, 'package.json');
+    if (_fs.default.existsSync(directory)) {
+      const packageJsonPath = _path.default.join(directory, 'package.json');
 
-			_jsonfile2.default.readFile(packageJsonPath, function (error, packageObj) {
-				if (error) {
-					reject('Path ' + directory + ' does not have a package.json file.');
-				} else {
-					if (!packageObj.hasOwnProperty('dependencies') && !packageObj.hasOwnProperty('devDependencies')) {
-						reject('There are no dependencies in the package.json file.');
-					}
+      _jsonfile.default.readFile(packageJsonPath, (error, packageObj) => {
+        if (error) {
+          reject(`Path ${directory} does not have a package.json file.`);
+        } else {
+          if (!packageObj.hasOwnProperty('dependencies') && !packageObj.hasOwnProperty('devDependencies')) {
+            reject('There are no dependencies in the package.json file.');
+          }
 
-					var dependencies = parseDependencies(packageObj);
-					var promises = [];
+          let dependencies = parseDependencies(packageObj);
+          let promises = [];
 
-					for (var i = 0; i < dependencies.length; i++) {
-						var dependency = processDependencies(dependencies[i], options);
-						promises.push(dependency);
-					}
+          for (let i = 0; i < dependencies.length; i++) {
+            const dependency = processDependencies(dependencies[i], options);
+            promises.push(dependency);
+          }
 
-					Promise.all(promises).then(function (values) {
-						resolve({ 'dependencies': values });
-					});
-				}
-			});
-		} else {
-			reject('Path ' + directory + ' does not exist.');
-		}
-	});
+          Promise.all(promises).then(values => {
+            resolve({
+              'dependencies': values
+            });
+          });
+        }
+      });
+    } else {
+      reject(`Path ${directory} does not exist.`);
+    }
+  });
 }
 
 function parseDependencies(packageObj) {
-	var parser = function parser(obj) {
-		var result = [];
-		for (var key in obj) {
-			if (obj.hasOwnProperty(key)) {
-				result.push({
-					package: key,
-					local: {
-						version: obj[key]
-					}
-				});
-			}
-		}
-		return result;
-	};
+  const parser = obj => {
+    let result = [];
 
-	var parsed = parser(packageObj.dependencies).concat(parser(packageObj.devDependencies));
-	return parsed;
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        result.push({
+          package: key,
+          local: {
+            version: obj[key]
+          }
+        });
+      }
+    }
+
+    return result;
+  };
+
+  const parsed = parser(packageObj.dependencies).concat(parser(packageObj.devDependencies));
+  return parsed;
 }
 
 function processDependencies(dependency, options) {
-	return new Promise(function (resolve, reject) {
-		var temp = '';
+  return new Promise((resolve, reject) => {
+    let temp = '';
+    const view = (0, _child_process.spawn)('npm', ['view', dependency.package, '--json']);
+    view.stdout.on('data', data => {
+      temp += data.toString();
+    });
+    view.on('close', code => {
+      const dependencyView = JSON.parse(temp);
+      const packageVersion = dependency.local.version;
 
-		var view = (0, _child_process.spawn)('npm', ['view', dependency.package, '--json']);
+      const localVersion = _semver.default.minSatisfying(dependencyView.versions, packageVersion);
 
-		view.stdout.on('data', function (data) {
-			temp += data.toString();
-		});
+      const wantedVersion = _semver.default.maxSatisfying(dependencyView.versions, packageVersion);
 
-		view.on('close', function (code) {
-			var dependencyView = JSON.parse(temp);
-
-			var packageVersion = dependency.local.version;
-			var localVersion = _semver2.default.minSatisfying(dependencyView.versions, packageVersion);
-			var wantedVersion = _semver2.default.maxSatisfying(dependencyView.versions, packageVersion);
-			var latestVersion = dependencyView.version;
-
-			var localDate = dependencyView.time[localVersion];
-			var wantedDate = dependencyView.time[wantedVersion];
-			var latestDate = dependencyView.time[latestVersion];
-
-			var wantedTimeDiff = localDate === wantedDate ? 0 : (0, _moment2.default)(wantedDate).valueOf() - (0, _moment2.default)(localDate).valueOf();
-			var latestTimeDiff = localDate === latestDate ? 0 : (0, _moment2.default)(latestDate).valueOf() - (0, _moment2.default)(localDate).valueOf();
-
-			dependency.wanted = {
-				version: wantedVersion,
-				time_diff: transform(wantedTimeDiff, options)
-			};
-
-			dependency.latest = {
-				version: latestVersion,
-				time_diff: transform(latestTimeDiff, options)
-			};
-
-			resolve(dependency);
-		});
-	});
+      const latestVersion = dependencyView.version;
+      const localDate = dependencyView.time[localVersion];
+      const wantedDate = dependencyView.time[wantedVersion];
+      const latestDate = dependencyView.time[latestVersion];
+      const wantedTimeDiff = localDate === wantedDate ? 0 : (0, _moment.default)(wantedDate).valueOf() - (0, _moment.default)(localDate).valueOf();
+      const latestTimeDiff = localDate === latestDate ? 0 : (0, _moment.default)(latestDate).valueOf() - (0, _moment.default)(localDate).valueOf();
+      dependency.wanted = {
+        version: wantedVersion,
+        time_diff: transform(wantedTimeDiff, options)
+      };
+      dependency.latest = {
+        version: latestVersion,
+        time_diff: transform(latestTimeDiff, options)
+      };
+      resolve(dependency);
+    });
+  });
 }
 
 function transform(value, options) {
-	if (options && options.h) return (0, _humanizeDuration2.default)(value, { round: true });
+  if (options && options.c) {
+    return (0, _humanizeDuration.default)(value, {
+      round: true,
+      units: ['y', 'mo', 'w', 'd']
+    });
+  } else if (options && options.f) {
+    return (0, _humanizeDuration.default)(value, {
+      round: true
+    });
+  }
 
-	return value;
+  return value;
 }
-module.exports = exports['default'];
+
+module.exports = exports["default"];

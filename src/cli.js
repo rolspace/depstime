44cc8b28983f68cf035d58da8f698e8bcee7f819
Roland <10880743+rolspace@users.noreply.github.com>
2024-06-05 21:33:52 +0200

    feat: Updated yargs implementation

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,19 @@
 import jsonfile from 'jsonfile'
 import util from 'util'
 import yargs from 'yargs'
-import { create, process } from './depstime.js'
+import { hideBin } from 'yargs/helpers'
+import * as depstime from './depstime.js'
 import { run } from './index.js'
 
 export default async function cli() {
   // eslint-disable-next-line prefer-destructuring
-  const options = yargs()
-    .usage('$0 [folder]')
+  const commandDefinition = yargs(hideBin(process.argv))
+    .command('$0 [directoryPath]', 'Calculate the time differences in your package.json dependencies', (yargs) => {
+      yargs.positional('[directoryPath]', {
+        describe: 'Path of the directory where the package.json file is located',
+        default: '.',
+      })
+    })
     .option('yarn', {
       describe: 'use yarn instead of npm',
       type: 'boolean',
@@ -15,28 +21,29 @@ export default async function cli() {
     .option('compact', {
       alias: 'c',
       describe:
-        'Output time difference as a human readable value up to the "days" unit with rounding',
+        'Output time data as a human readable value up to the "days" unit with rounding',
       type: 'boolean',
     })
     .option('full', {
       alias: 'f',
       describe:
-        'Output time difference as a human readable value up to the "seconds" unit with rounding',
+        'Output time data as a human readable value up to the "seconds" unit with rounding',
       type: 'boolean',
     })
-    .help('help', 'Show help message').argv
+    .help('help', 'Show help message')
+    .parse()
 
   try {
     // eslint-disable-next-line prefer-destructuring
-    const [folder] = options._
+    const [directoryPath] = commandDefinition._
 
     const { readFile } = jsonfile
     const runDependencies = {
       readPackageFile: readFile,
-      createDepstime: create,
-      processDepstimes: process,
+      createDepstime: depstime.create,
+      processDepstimes: depstime.process,
     }
-    const result = await run(folder, options, runDependencies)
+    const result = await run(directoryPath, commandDefinition, runDependencies)
 
     console.log(util.inspect(result, { colors: true, depth: null }))
   } catch (error) {
